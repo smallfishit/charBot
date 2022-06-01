ada9e313d0c45785cc099a6e6acf03ad62d6d1a5
smallfishit <henrychang0202@gmail.com>
2022-06-01 20:57:09 +0800

    快要完成電腦版頁面

--- JS/all.js
+++ JS/all.js
@@ -3,3 +3,25 @@ $(".top").click(function() {
         scrollTop: 0
     }, 1000);
 });
+
+function ChangProgram()
+{
+    location.href = 'program.html'
+}
+
+function ChangIndex()
+{
+    location.href = 'index.html'
+}
+
+$(".circle_button_normal2").click(function () {
+    if($(this).hasClass('white_green_button'))
+    {
+        $(this).removeClass('white_green_button').addClass('green_button');
+        $(this).parent().siblings().find('button').addClass('white_green_button').removeClass('green_button');
+    }
+});
+
+$(".content_problem_symbol").click(function() {
+    console.log('測試');
+});
