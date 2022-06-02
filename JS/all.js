8e73d6cde7b0985635a0375f75c7b0dd36038e42
smallfishit <henrychang0202@gmail.com>
2022-06-02 16:27:30 +0800

    完成問題修正並完成畫面一的RWD

--- JS/all.js
+++ JS/all.js
@@ -1,4 +1,5 @@
 $(".top").click(function() {
+    console.log('回到最上');
     jQuery("html,body").animate({
         scrollTop: 0
     }, 1000);
@@ -23,5 +24,14 @@ $(".circle_button_normal2").click(function () {
 });
 
 $(".content_problem_symbol").click(function() {
-    console.log('測試');
+    let btn = $(this);
+    btn.toggleClass("icon_minus").toggleClass("icon_plus");
+    btn.parent().toggleClass("active");
+    let item = btn.parent().parent();
+    item.find(".content_problem_answer").toggle();
+
+    //關閉其他的頁籤
+    item.siblings().find(".content_problem_answer").hide();
+    item.siblings().find(".content_problem_symbol").removeClass("icon_minus").addClass("icon_plus");
+    item.siblings().find(".content_problem_question").removeClass("active");
 });
